1cba0c91846545ad31162093899076f72e03486f
Tornado4651 <tornado4651@163.com>
2020-05-08 13:47:55 +0800

    新增链表部分

---
+++ DataStructures/src/com/tornado4651/data_structures/linkedlist/DoubleLinkedListDemo.java
@@ -0,0 +1,19 @@
+package com.tornado4651.data_structures.linkedlist;
+
+public class DoubleLinkedListDemo {
+
+}
+
+
+class DoubleNode{
+    DoubleNode pre;
+    Integer id;
+    String name;
+    String password;
+    DoubleNode next;
+}
+
+
+class DoubleLinkedList{
+
+}
---
+++ DataStructures/src/com/tornado4651/data_structures/linkedlist/Josepfu.java
@@ -0,0 +1,24 @@
+package com.tornado4651.data_structures.linkedlist;
+
+
+/**
+ * 约瑟夫（环）问题：
+ *      在围坐一圈的m个学生中，从第k个人开始报数n次
+ *      最后报数的人出圈
+ *      求出圈人的顺序
+ * 使用单项循环连标
+ */
+public class Josepfu {
+
+}
+
+
+class StuNode{
+    Integer id;
+    String name;
+    StuNode next;
+}
+
+class CircleSingleLinkedList{
+
+}
