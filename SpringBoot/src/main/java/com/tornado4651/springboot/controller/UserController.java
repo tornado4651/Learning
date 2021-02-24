e4d04f4730bf81531fdea2ddf71ae7b153f1cb1b
tornado4651 <tornado4651@163.com>
2021-02-24 23:09:45 +0800

    新增SpringBoot学习模块

--- SpringBoot/src/main/java/com/tornado4651/springboot/controller/UserController.java
+++ SpringBoot/src/main/java/com/tornado4651/springboot/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.tornado4651.springboot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
+    @RequestMapping("hello")
+    public String hello() {
+        return "hello,world";
+    }
+
 }
